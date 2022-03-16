56068354db4418151cbbb2ba3c7ca444b18ae57b
DanieleFic <reb_ficus22@hotmail.it>
2022-03-16 18:05:18 +0100

    fatto menu nella dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,8 +56,8 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -73,6 +73,16 @@
         </nav>
 
         <main class="py-4">
+
+            <div class="ms_container">
+                <div>
+                    <a href="{{route("admin.home")}}"><i class="bi bi-house-door-fill"></i>Home</a>
+                </div>
+                <div>
+                    <a href="{{route("admin.posts.index")}}"><i class="bi bi-list-ul"></i>Lista Post</a>
+                </div>
+            </div>
+            
             @yield('content')
         </main>
     </div>
